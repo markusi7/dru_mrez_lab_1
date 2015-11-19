b58ef85c70dc2b241994154dfaa5a70c74426ba9
markusi <ivan.markusi@infinum.hr>
2015-11-20 00:36:11 +0100

    added songs index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
 
+  resources :songs
+
   root to: "users#index"
 end
