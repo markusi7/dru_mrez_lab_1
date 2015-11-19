e2bcee9ca1e5cff73eef08a4b076fe6bf8488379
markusi <ivan.markusi@infinum.hr>
2015-11-20 00:12:39 +0100

    finished storing user to db

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,19 @@ class User
   include Mongoid::Attributes::Dynamic
   include Mongoid::Document
 
+  field :uid, type: String
+  field :name, type: String
+  field :provider, type: String
+  field :image, type: String
+  field :email, type: String
+
   def self.omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-      byebug
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
       user.image = auth.info.image
+      user.email = auth.info.email
       user.save!
     end
   end
